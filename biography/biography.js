e03b593f0680b232ae118e4f3e5fc858af354b39
Ryan Cheung <ryan_cheung@hotmail.co.uk>
2023-02-11 18:03:40 +0000

    JS Data for biography, appearance, power stats complete and appended to page, fonts added to html and incorporated, JS added to empty data from page when searching for new hero/villain, appropriate css styling added.

--- biography/biography.js
+++ biography/biography.js
@@ -11,10 +11,11 @@ APIkey = "10220548467573520";
 function superheroIndex() {
   searchBtn.on("click", function (event) {
     event.preventDefault();
-    var findHero = userInput.val();
+    biographyContainer.empty()
+    var findHero = userInput.val().trim();
     var queryURL =
       "https://www.superheroapi.com/api.php/" + APIkey + "/search/" + findHero;
-
+      
     $.ajax({
       url: queryURL,
       method: "GET",
@@ -29,13 +30,21 @@ function superheroIndex() {
           "https://via.placeholder.com/240x320.png";
         var img = $("<img>").attr("src", superheroImg).addClass("img-fluid");
 
-        // Create biography info.
+        
         var superHeroName = $("<h2>")
           .text(response.results[i].name)
-          .addClass("text-center font-weight-bold text-uppercase");
+          .addClass("text-center font-weight-bold text-uppercase info");
+
+        var dataDiv = $("<div>")
+          .attr("id", "div-styling")
+          .addClass("border div-styling");
+        biographyContainer.append(img, superHeroName);
+        
+        // Create biography info.
         var biography = $("<ul>")
           .text("Biography")
-          .addClass("text-center font-weight-bold");
+          .addClass("text-center font-weight-bold underline")
+
         var fullname = $("<li>").text(
           "Full name:" + " " + response.results[i].biography["full-name"]
         );
@@ -52,9 +61,8 @@ function superheroIndex() {
         var publisher = $("<li>").text(
           "Publisher:" + " " + response.results[i].biography.publisher
         );
-        biographyContainer.append(
-          img,
-          superHeroName,
+        biographyContainer.append(dataDiv);
+        dataDiv.append(
           biography,
           fullname,
           placeofBirth,
@@ -65,7 +73,8 @@ function superheroIndex() {
         // Create appearance info.
         var appearance = $("<ul>")
           .text("Appearance")
-          .addClass("text-center font-weight-bold");
+          .addClass("text-center font-weight-bold underline")
+
         var gender = $("<li>").text(
           "Gender:" + " " + response.results[i].appearance.gender
         );
@@ -81,27 +90,25 @@ function superheroIndex() {
         var weight = $("<li>").text(
           "Weight:" + " " + Object.values(response.results[i].appearance.weight)
         );
-        biographyContainer.append(
-          appearance,
-          gender,
-          hairColor,
-          race,
-          height,
-          weight
-        );
+        dataDiv.append(appearance, gender, hairColor, race, height, weight);
 
+        // Create
+        
+
+        // Create Powerstats info.
         var powerStats = $("<ul>")
           .text("Power Stats")
-          .addClass("text-center font-weight-bold");
-        biographyContainer.append(powerStats);
+          .addClass("text-center font-weight-bold underline");
+        dataDiv.append(powerStats);
 
         const object = response.results[i].powerstats;
 
         for (const property in object) {
-          biographyContainer.append(`${property}: ${object[property]} , `);
+          dataDiv.append(`${property}: ${object[property]} , `);
         }
       }
     });
   });
 }
 superheroIndex();
+
