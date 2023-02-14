1ea2be6b84312e103c89365ebbb7cc0c220c7d2e
SIR-97 <sirafiq.1997@gmail.com>
2023-02-14 19:33:37 +0000

    used js and css to set styling for my nav with hori selector that fully functions

--- assets/js/script.js
+++ assets/js/script.js
@@ -479,15 +479,51 @@ console.log("game over: ", gameOver);
 	};
 });
 
-// toggle the navigation start
+//navigation start
 
-$(document).ready(function() {
-	$(".navbar-toggler").click(function() {
-	  $("#navbarNav").toggle();
-	});
-  });
+
   
-  // toggle naivgation end
+function test(){
+	var tabsNewAnim = $('#navbarSupportedContent');
+	var selectorNewAnim = $('#navbarSupportedContent').find('li').length;
+	var activeItemNewAnim = tabsNewAnim.find('.active');
+	var activeWidthNewAnimHeight = activeItemNewAnim.innerHeight();
+	var activeWidthNewAnimWidth = activeItemNewAnim.innerWidth();
+	var itemPosNewAnimTop = activeItemNewAnim.position();
+	var itemPosNewAnimLeft = activeItemNewAnim.position();
+	$(".hori-selector").css({
+		"top":itemPosNewAnimTop.top + "px", 
+		"left":itemPosNewAnimLeft.left + "px",
+		"height": activeWidthNewAnimHeight + "px",
+		"width": activeWidthNewAnimWidth + "px"
+	});
+	$("#navbarSupportedContent").on("click","li",function(e){
+		$('#navbarSupportedContent ul li').removeClass("active");
+		$(this).addClass('active');
+		var activeWidthNewAnimHeight = $(this).innerHeight();
+		var activeWidthNewAnimWidth = $(this).innerWidth();
+		var itemPosNewAnimTop = $(this).position();
+		var itemPosNewAnimLeft = $(this).position();
+		$(".hori-selector").css({
+			"top":itemPosNewAnimTop.top + "px", 
+			"left":itemPosNewAnimLeft.left + "px",
+			"height": activeWidthNewAnimHeight + "px",
+			"width": activeWidthNewAnimWidth + "px"
+		});
+	});
+}
+$(document).ready(function(){
+	setTimeout(function(){ test(); });
+});
+$(window).on('resize', function(){
+	setTimeout(function(){ test(); }, 500);
+});
+$(".navbar-toggler").click(function(){
+	$(".navbar-collapse").slideToggle(300);
+	setTimeout(function(){ test(); });
+});
+
+  // naivgation end
 
   
   // modal for about us
@@ -511,6 +547,8 @@ $(document).ready(function() {
 	  }
 	});
   });
+
+  
   
 
   // modal for how to play 
